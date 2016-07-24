42bd8f74b113e8059f9e3542469bcabd4704d0ed
artman202 <eti.strydom@gmail.com>
2016-07-24 17:55:22 +0200

    Etienne Laptop Windows

--- app/app.ts
+++ app/app.ts
@@ -30,6 +30,9 @@ export class MyApp {
   private rootPage: any;
 
   constructor(private platform: Platform, private menu:MenuController) {
+    
+    this.rootPage = LoginPage;
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
